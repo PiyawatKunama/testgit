662c596e62f34af2e477041cffcb7eab7faca68e
PiyawatKunama <69465266+PiyawatKunama@users.noreply.github.com>
2020-11-25 15:13:02 +0700

    user_article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,11 @@ class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
   def new
-    @article = Article.new
+    @article = current_user.articles.new
   end
 
   def edit
-    @article = Article.find(params[:id])
+    @article = current_user.articles.find(params[:id])
   end
 
 
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
+    @article = current_user.articles.find(params[:id])
    
     if @article.update(article_params)
       redirect_to @article
@@ -45,12 +45,12 @@ class ArticlesController < ApplicationController
 
     
   def show
-    @article = Article.find(params[:id])
+    @article = current_user.articles.find(params[:id])
   end
 
 
   def destroy
-    @article = Article.find(params[:id])
+    @article = current_user.articles.find(params[:id])
     @article.destroy
    
     redirect_to(articles_path)
